834f0a3b63a686f0beafe693941923df446a766a
AlbanP <no email>
2024-11-27 20:57:12 +0100

    Requires Python >= 3.6 and <3.11

--- examples/web_search.py
+++ examples/web_search.py
@@ -17,20 +17,25 @@ class WebSearch(Noesis):
         task:Information = f"{self.request}"
         current_date = date.today().strftime("%d-%m-%Y")
         current_date_info:Information = f"The current date is: {current_date}"
-        knowledge_reflexion:Fill = ("Thinking about the task.",
-                                f"""I have to think about the task: '{task.value}'.
-                                Based on the date and my knowledge, can I Know the answer? {Bool:known_answer}.
-                                """)
+        knowledge_reflexion:Fill = (
+            "Thinking about the task.",
+            f"""I have to think about the task: '{task.value}'.
+            Based on the date and my knowledge, can I Know the answer? {Bool:known_answer}.
+            """
+        )
+    
         if knowledge_reflexion.known_answer:
             answer:Sentence = "Producing the answer."
         else:
             search_results = google_search(task.value)
             results:Information = f"The search results are: {search_results}"
-            manage_results:Fill = ("Managing the search results.",
-                                    f"""Selecting the best result: {SubString:infos}.
-                                    Extracting the best link: {SubString:link}
-                                    Producing the answer based on the information: {Sentence:answer}.
-                                    """)
+            manage_results:Fill = (
+                "Managing the search results.",
+                f"""Selecting the best result: {SubString:infos}.
+                Extracting the best link: {SubString:link}
+                Producing the answer based on the information: {Sentence:answer}.
+                """
+            )
             elaborate:Paragraph = "Using the information of the selected result, I elaborate the answer."
             return elaborate.value, manage_results.link
             
